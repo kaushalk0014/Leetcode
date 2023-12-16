cde6b6c4d7cb6671d3912e24b9842dfa30014f5b
kaushal Kumar <151421895+kaushalk0014@users.noreply.github.com>
2023-12-16 09:50:08 +0530

    Time: 1 ms (94.56%), Space: 44 MB (30.86%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,32 @@
+class Solution {
+    List<List<Integer>> ans=new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        dfs(nums,0);
+        return ans;
+    }
+    private void dfs(int[] nums,int index){
+            if(index==nums.length){
+                ans.add(toList(nums));
+                return;
+            }else{
+                 for(int i=index;i<nums.length;i++){
+                    swap(i,index,nums);
+                    dfs(nums,index+1);
+                    swap(i,index,nums);
+                }
+            }
+    }
+    private void swap(int x,int y, int []nums){
+        int temp=nums[x];
+        nums[x]=nums[y];
+        nums[y]=temp;
+    }
+    
+    private List<Integer> toList(int[] nums){
+        List<Integer> list=new ArrayList<>();
+        for(int value:nums){
+            list.add(value);
+        }
+        return list;
+    }
+}
